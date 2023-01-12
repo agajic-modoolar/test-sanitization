0163ee99608096c70f4c40b962620a8b9fa9f8ff
Aleksandar Gajic <aleksandar.gajic@modoolar.com>
2023-01-12 13:51:19 +0100

    fix

--- test_bootstrap/__init__.py
+++ test_bootstrap/__init__.py
@@ -2,6 +2,10 @@ import logging
 from odoo import models
 
 _logger = logging.getLogger(__name__)
+
+
 class Sanitize(models.AbstractModel):
+    _name = "sanitization"
+
     def init(self):
         _logger.error("SANITIZATION")
